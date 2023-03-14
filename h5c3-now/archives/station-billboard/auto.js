acc96a5857ee66e0b67a23ae3159c83fbbab2ba7
Penyo <penyoofficial@outlook.com>
2023-03-14 19:43:40 +0800

    RTM23073
    - 修复 前端演示 中 电车站公告板 的大量问题。

---
+++ h5c3-now/archives/station-billboard/auto.js
@@ -0,0 +1,6 @@
+addEvent("19:34", 4, "上海", "SHANGHAI", 2, "G7278", true);
+addEvent("19:35", 3, "重庆西", "CHONGQINXI", 1, "K1153", false);
+addEvent("19:45", 3, "上海虹桥", "SHANGHAIHONGQIAO", 2, "G3182");
+addEvent("19:50", 1, "巢湖东", "CHAOHUDONG", 2, "G7256");
+addEvent("19:56", 2, "杭州东", "HANGZHOUDONG", 2, "G3194");
+addEvent("20:04", 5, "淮北", "HUAIBEI", 2, "G7410");
