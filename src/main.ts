9ba355881a70dbbda98b0c399f0101094439a650
Penyo <penyoofficial@outlook.com>
2025-06-25 21:12:10 +0800

    to

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,32 @@ import { createPinia } from "pinia";
 import App from "@/App.vue";
 import router from "@/router";
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+
 const app = createApp(App);
 
-app.use(createPinia());
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia);
+
 app.use(router);
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    aliases,
+    sets: {
+      mdi,
+    },
+  }
+})
+app.use(vuetify);
 
 app.mount("#app");
